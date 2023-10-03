0a8bf257891abb1df9c320c981d45a84868cf15c
Carolina Turner <carolinanturner@gmail.com>
2023-10-02 21:49:04 -0400

    addded assignment 8!

--- assignments/assignment 8/script.js
+++ assignments/assignment 8/script.js
@@ -2,20 +2,33 @@
 
 const move = () => {
     let displacement = 0;
-    let turtle = document.querySelector("turtle");
+    const t = document.querySelector("turtle");
     let interval = setInterval(() => {
-      displacement ++;
-      document.getElementById("turtle").style.setProperty("--left-margin", displacement, "px");
-      if (displacement >= 100) {
+      displacement +=5;
+      document.getElementById("turtle").style.setProperty("--left-margin", displacement + "px");
+      if (displacement >= 550) {
         clearInterval(interval);
       }
-    }, 1);
+    }, 100);
 }
-
-
-
+  const fillBox = () => {
+    const input = document.getElementById("txt-goal").value;
+    const root = document.querySelector(":root");
+    let int = 0;
+    if(int<input/100){
+        let interval = setInterval(() => {
+            const fill = "pink " + int + "%";
+            root.style.setProperty("--pink", fill)
+            root.style.setProperty("--white","white 0%")
+            int++;
+            if(int>=input/100){
+                clearInterval(interval);
+            }
+        },50);
+    }
+}
  window.onload = () =>{ 
   document.getElementById("walking-turtle").onclick = move;
- 
+  document.getElementById("display").onclick=fillBox;
  
  }
